0577fd795986233735ccaf78ae33702b9d38b7e1
jbmdevelopment <jbarreramartinez100@student.hbuhsd.edu>
2024-10-25 10:55:02 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -10,9 +10,8 @@ public class Main
     boolean isRotated = (rec1.getLength() == rec2.getWidth()) && (rec1.getWidth() == rec2.getLength());
     boolean isCongruent = (isRotated || rec1.equals(rec2));
     boolean isSimilar = isCongruent || ( (rec1.getLength() / rec2.getLength()) == (rec1.getWidth() / rec2.getWidth()));
-	  System.out.println(isRotated);
+    System.out.println(isRotated);
     System.out.println(isCongruent);
     System.out.println(isSimilar);
-    // write solutions to problems here
   }
 }
